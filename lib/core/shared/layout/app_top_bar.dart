4cd015e21961188ddf84e55d6abcf2369f6cf086
MoEhab74 <mohamedehabswe74@gmail.com>
2026-07-19 02:51:12 +0300

    feat: implement profile view, shared top bar, typography styles, and configure project plugins

--- lib/core/shared/layout/app_top_bar.dart
+++ lib/core/shared/layout/app_top_bar.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hugeicons/hugeicons.dart';
 
-import '../../constants/app_assets.dart';
-import '../../theme/colors/app_colors.dart';
 
 class AppTopBar extends StatelessWidget implements PreferredSizeWidget {
   final VoidCallback? onMenuTap;
   final VoidCallback? onProfileTap;
+  final Widget? actionWidget;
+  final Widget? leadingWidget;
 
   const AppTopBar({
     super.key,
     this.onMenuTap,
     this.onProfileTap,
+    this.actionWidget,
+    this.leadingWidget,
   });
 
   @override
@@ -23,29 +24,11 @@ class AppTopBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Colors.transparent,
       elevation: 0,
       surfaceTintColor: Colors.transparent,
-      leadingWidth: 56.w,
-      leading: IconButton(
-        onPressed: onMenuTap,
-        icon: HugeIcon(
-          icon: HugeIcons.strokeRoundedMenu05,
-          size: 28.r,
-          color: AppColors.primary,
-        ),
-      ),
-      title: Image.asset(
-        AppAssets.appBarLogo,
-        height: 32.h,
-        fit: BoxFit.contain,
-      ),
+      leadingWidth: 120.w,
+      // ProfileImage circle avatar
+      leading: leadingWidget ?? SizedBox.shrink(),
       actions: [
-        IconButton(
-          onPressed: onProfileTap,
-          icon: HugeIcon(
-            icon: HugeIcons.strokeRoundedUserSquare,
-            color: AppColors.primary,
-            size: 28.r,
-          ),
-        ),
+        actionWidget ?? SizedBox.shrink(),
         SizedBox(width: 8.w),
       ],
     );
