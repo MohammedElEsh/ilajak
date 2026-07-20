8728ffa652d20b97b21a531c5db120b6ceec4c9e
Sabri <sabrywaleedsw@gmail.com>
2026-07-20 20:54:53 +0300

    feat: add ConditionsReportWidget to display medical condition summaries on the home screen

--- lib/features/home/presentation/widgets/conditions_report_widget.dart
+++ lib/features/home/presentation/widgets/conditions_report_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hugeicons/hugeicons.dart';
-import 'package:ilajak/core/constants/app_assets.dart';
 import 'package:ilajak/core/theme/colors/app_colors.dart';
 import 'package:ilajak/core/theme/typography/app_typography.dart';
 
